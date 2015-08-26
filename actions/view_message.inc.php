0c42887e7c9bc3e0388bc8694fce3a17886adc83
Andrey Mukha <andryfly7@gmail.com>
2015-08-26 19:56:58 +0300

    Изменил вывод ошибки при отсутсвии статьи или доступа к ней

--- actions/view_message.inc.php
+++ actions/view_message.inc.php
@@ -6,6 +6,7 @@ if($_SERVER['REQUEST_METHOD'] == 'GET' and $_GET['action'] == 'view_message' and
 	$additional_images = explode('|', $message['additional_images']);
 
 	if($user['user_id'] != $message['post_uid'] and $message['published'] == 0){
+		$_SESSION['msg']['message'] = setMessage('Данное объявление не существует', 'error');
 		$content = template('view_message.tpl.php');
 	}
 	else{
@@ -13,6 +14,7 @@ if($_SERVER['REQUEST_METHOD'] == 'GET' and $_GET['action'] == 'view_message' and
 	}
 
 }else{
+	$_SESSION['msg']['message'] = setMessage('Данное объявление не существует', 'error');
 	$content = template('view_message.tpl.php');
 }
 
